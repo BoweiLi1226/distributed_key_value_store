691900524f565c5d1e811b0a066ca10ad60c764e
Bowei Li <cesterty024@gmail.com>
2026-02-21 11:49:32 -0500

    Remove unused imports for tests

--- tests/kv_store/shared_kv_store_test.cpp
+++ tests/kv_store/shared_kv_store_test.cpp
@@ -1,18 +1,8 @@
 #include "kv_store/shared_kv_store.h"
-#include "kv_store/utils.h"
 #include <boost/asio/any_io_executor.hpp>
 #include <boost/asio/awaitable.hpp>
 #include <boost/asio/co_spawn.hpp>
-#include <boost/asio/deferred.hpp>
-#include <boost/asio/detached.hpp>
-#include <boost/asio/dispatch.hpp>
 #include <boost/asio/executor_work_guard.hpp>
-#include <boost/asio/experimental/awaitable_operators.hpp>
-#include <boost/asio/experimental/cancellation_condition.hpp>
-#include <boost/asio/experimental/parallel_group.hpp>
-#include <boost/asio/io_context.hpp>
-#include <boost/asio/post.hpp>
-#include <boost/asio/strand.hpp>
 #include <boost/asio/this_coro.hpp>
 #include <boost/asio/thread_pool.hpp>
 #include <boost/asio/use_awaitable.hpp>
@@ -24,11 +14,11 @@
 
 using distributed_key_value_store::kv_store::SharedKVStore;
 
-static constexpr int WORKER_THREADS = 7;
+static constexpr int WORKER_THREADS = 6;
 static constexpr int TASKS = 300;
 
 TEST(SharedKVStoreTests, BasicPutGet) {
-  boost::asio::thread_pool thread_pool(6);
+  boost::asio::thread_pool thread_pool(WORKER_THREADS);
   auto guard = boost::asio::make_work_guard(thread_pool);
   SharedKVStore data_store(thread_pool.get_executor());
 
@@ -44,6 +34,10 @@ TEST(SharedKVStoreTests, BasicPutGet) {
               [&data_store, i] -> boost::asio::awaitable<void> {
                 auto _ =
                     co_await data_store.put(std::format("bowei{}", i), "yushi");
+
+                auto result =
+                    co_await data_store.get(std::format("bowei{}", i));
+                EXPECT_EQ("yushi", result.value().value);
               },
               boost::asio::use_awaitable));
         }
@@ -53,18 +47,8 @@ TEST(SharedKVStoreTests, BasicPutGet) {
         }
       },
       boost::asio::use_future);
-  puts.get();
 
-  auto gets = boost::asio::co_spawn(
-      thread_pool,
-      [&data_store]() -> boost::asio::awaitable<void> {
-        for (int i = 0; i < TASKS; ++i) {
-          auto result = co_await data_store.get(std::format("bowei{}", i));
-          EXPECT_EQ("yushi", result.value().value);
-        }
-      },
-      boost::asio::use_future);
-  gets.get();
+  puts.get();
 
   guard.reset();
 }
